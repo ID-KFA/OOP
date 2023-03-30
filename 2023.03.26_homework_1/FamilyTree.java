3da62bfc4dab29f2e244534b39a076a29b5abc19
idkfa <1@idkfa.ru>
2023-03-30 23:23:29 +0500

    ДЗ 1 исправлено по замечаниям

--- 2023.03.26_homework_1/FamilyTree.java
+++ 2023.03.26_homework_1/FamilyTree.java
@@ -1,59 +1,71 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class FamilyTree{
-    private List <Human> humans;
-    private Human result;
-    
-   
-    public FamilyTree(){
-        this (new ArrayList());
+public class FamilyTree {
+    private List<Human> humans;
+
+    public FamilyTree() {
+        this(new ArrayList());
 
     }
 
-    private FamilyTree (List<Human> humans){
-        this.humans=humans;
+    private FamilyTree(List<Human> humans) {
+        this.humans = humans;
     }
 
-    public void add (Human h){
+    public void add(Human h) {
         humans.add(h);
 
     }
 
-    public List getInfo (){
-        List res=new ArrayList();
+    public List getInfo() {
+        List res = new ArrayList();
         String str;
         for (int i = 0; i < humans.size(); i++) {
-            str=humans.get(i).getName() +" - "+ humans.get(i).getGender();
-            if (humans.get(i).father!= null) str+="; "+humans.get(i).getFather()+";";
-            if (humans.get(i).mother!= null) str+=" "+humans.get(i).getMother();
+            str = humans.get(i).toString();
             res.add(str);
 
-            
         }
         return res;
 
-   
-
-    }    
+    }
     // public Human getByName(String name){
- 
-    //     for (int i = 0; i < humans.size(); i++) {
-    //         if (this.humans.get(i).getName().equals(name)) 
-    //           result=this.humans.get(i);
-                
-                       
-    //     }
-    //     return result;
 
-        
+    // for (int i = 0; i < humans.size(); i++) {
+    // if (this.humans.get(i).getName().equals(name))
+    // result=this.humans.get(i);
+
+    // }
+    // return result;
+
     // }
 
-    
+    public List<Human> getKids(String name) {
 
-        
-    
+        List<Human> kids = new ArrayList();
+        for (int i = 0; i < humans.size(); i++) {
 
+            if (this.humans.get(i).dadOf() != null) {
+
+                if (this.humans.get(i).dadOf().getName().equals(name)) {
+
+                    kids.add(this.humans.get(i));
+                }
+
+            }
+
+            if (this.humans.get(i).momOf() != null) {
+
+                if (this.humans.get(i).momOf().getName().equals(name)) {
+
+                    kids.add(this.humans.get(i));
+                }
+
+            }
+
+        }
+        return kids;
+
+    }
 
 }
-
--- 2023.03.26_homework_1/Human.java
+++ 2023.03.26_homework_1/Human.java
@@ -1,46 +1,89 @@
+
+import java.util.List;
+
 public class Human {
-String name;
-Gender gender;
-Human father;
-Human mother;
+    private String name;
+    private Gender gender;
+    private Human father;
+    private Human mother;
+    List<Human> kids;
 
+    public Human(String name, Gender gender, Human father, Human mother, List<Human> kids) {
+        this.name = name;
+        this.gender = gender;
+        this.father = father;
+        this.mother = mother;
+        this.kids = kids;
+    }
 
+    public Human(String name, Gender gender, Human father, Human mother) {
+        this.name = name;
+        this.gender = gender;
+        this.father = father;
+        this.mother = mother;
 
-public Human(String name, Gender gender, Human father, Human mother){
-    this.name =name;
-    this.gender=gender;
-    this.father=father;
-    this.mother=mother;
-}
+    }
 
-public Human(String name, Gender gender){
-    this.name =name;
-    this.gender=gender;
-    
-}
+    public Human(String name, Gender gender, List<Human> kids) {
+        this.name = name;
+        this.gender = gender;
+        this.kids = kids;
 
+    }
 
-public String getName(){
-    return name;
-    
-}
+    public Human(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
 
-public Gender getGender(){
-    return gender;
-}
-public String getFather(){
-    return "father: "+father.toString();
-}
-public String getMother(){
-    return "mother: "+mother.toString();
-}
+    }
 
+    public String getName() {
+        return name;
 
+    }
 
-@Override
-public String toString() {
-    
-    return name;
-}
+    public Gender getGender() {
+        return gender;
+
+    }
+
+    public Human dadOf() {
+        return father;
+
+    }
+
+    public Human momOf() {
+        return mother;
+
+    }
+
+    public String getFather() {
+        return "father: " + father.toString();
+    }
+
+    public String getMother() {
+        return "mother: " + mother.toString();
+    }
+
+    public String getKids() {
+        return "kids: " + kids.toString();
+
+    }
+
+    @Override
+    public String toString() {
+
+        String str;
+
+        str = getName() + " - " + getGender();
+        if (father != null)
+            str += "; " + getFather() + ";";
+        if (mother != null)
+            str += " " + getMother();
+        if (kids != null)
+            str += " " + getKids();
+
+        return str;
+    }
 
 }
